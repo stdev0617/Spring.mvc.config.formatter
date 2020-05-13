ef774b9494f5b4aac8ead8ea7b18341b87fc350c
stdev0617 <stdev0617@gmail.com>
2020-05-13 23:38:59 +0900

    updated person entity contents

--- src/main/java/me/stdev/demobootweb/Person.java
+++ src/main/java/me/stdev/demobootweb/Person.java
@@ -1,8 +1,20 @@
 package me.stdev.demobootweb;
 
+import java.util.Date;
+
 public class Person {
 
+    private Long id;
+
     private String name;
+
+    private String address;
+
+    private String email;
+
+    private Date createdDate;
+
+    private Date modifiedDate;
 
     public String getName() {
         return name;
@@ -10,5 +22,45 @@ public class Person {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setModifiedDate(Date modifiedDate) {
+        this.modifiedDate = modifiedDate;
     }
 }
--- src/main/java/me/stdev/demobootweb/WebConfig.java
+++ src/main/java/me/stdev/demobootweb/WebConfig.java
@@ -2,6 +2,7 @@ package me.stdev.demobootweb;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -10,5 +11,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addFormatter(new PersonFormatter());
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
     }
 }
